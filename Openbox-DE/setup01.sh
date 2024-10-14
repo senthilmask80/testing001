b882cbd599535d20a7b9a8e7daf2e11f6192f165
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-14 17:36:43 +0530

    Update setup01.sh

--- Openbox-DE/setup01.sh
+++ Openbox-DE/setup01.sh
@@ -46,8 +46,8 @@ then
 elif [ -x "$(command -v apt-get)" ];
 then
     sudo apt-get -y update && sudo apt-get -y upgrade
-    echo "$PROXPACK"
-    sudo apt-get -y --ignore-missing install $(< $PROXPACK/debian-packages.list)
+    echo "${PROXPACK}"
+    sudo apt-get -y --ignore-missing install $(< ${PROXPACK}/debian-packages.list)
 elif [ -x "$(command -v dnf)" ];
 then
     sudo dnf install "${packagesNeeded[@]}"
