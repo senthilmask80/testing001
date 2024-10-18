bb0c88dff819e56f3abbe839b394d25f0b5cad8e
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-18 12:54:42 +0530

    Update webkit2.sh

--- Openbox-DE/Scripts/webkit2.sh
+++ Openbox-DE/Scripts/webkit2.sh
@@ -1,25 +1,30 @@
 #! /bin/bash
 
-sudo apt install libssl-dev
+if command_exists nvm; then
+  echo "NVM and NodeJS already installed"
+  return
+fi
+  # you already have what is necessary on your system to install NVM. To do this, perform the following step:
+if ! curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash; then 
+  echo "Successfully NVM and NodeJS installed"
+  return
+else
+  source ~/.bashrc
 
-# you already have what is necessary on your system to install NVM. To do this, perform the following step:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+  # I chose the latest LTS version, by the time I'm writing this * Gist *, it's v8.9.1. You can install it by typing:
+  nvm install v8.9.1
 
-source ~/.bashrc
+  # To set a version of the node as default, run the following command:
+  nvm alias default 8.9.1
 
-# I chose the latest LTS version, by the time I'm writing this * Gist *, it's v8.9.1. You can install it by typing:
-nvm install v8.9.1
+  # Installing the vue-cli
+  npm install -g vue-cli
 
-# To set a version of the node as default, run the following command:
-nvm alias default 8.9.1
+  npm install -g vue-cli --force
 
-# Installing the vue-cli
-npm install -g vue-cli
+  # install the lightdm-webkit2-greeter themes Saluto
+  git clone https://github.com/Demonstrandum/Saluto.git /tmp/Saluto/
 
-npm install -g vue-cli --force
-
-# install the lightdm-webkit2-greeter themes Saluto
-git clone https://github.com/Demonstrandum/Saluto.git /tmp/Saluto/
-
-cd /tmp/Saluto/
-sh ./install.sh
+  cd /tmp/Saluto/
+  sh ./install.sh
+fi
