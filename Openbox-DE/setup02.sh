ac665446b0e90c3aec22306a543168acdb8fa5b2
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-18 09:43:40 +0530

    Update setup02.sh

--- Openbox-DE/setup02.sh
+++ Openbox-DE/setup02.sh
@@ -1,1 +1,43 @@
+!/bin/bash
 
+RC='\033[0m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+# Check if the opt directory and Proxmox-DE folder exist, create them if they don't
+# add variables to top level so can easily be accessed by all functions
+
+PROXDIR="/opt/Proxmox-DE"
+SUDO_CMD=""
+SUGROUP=""
+GITPATH="$PROXDIR/Openbox-DE/Openbox-DE/bash"
+
+mkdir -p ~/.local/bin/
+mkdir -p ~/.config/obmenu-generator
+mkdir -p ~/.config/fbmenugen
+mkdir -p /usr/share/backgrounds
+mkdir -p $HOME/.nvm
+
+if [ ! -d "$PROXDIR" ]; then
+    echo "$YELLOW Creating Proxmox-DE directory: $PROXDIR $RC"
+    mkdir -p "$PROXDIR"
+    echo "$GREEN Proxmox-DE directory created: $PROXDIR $RC"
+fi
+
+if [ -d "$PROXDIR/Openbox-DE" ]; then rm -rf "$PROXDIR/Openbox-DE"; fi
+	echo "$YELLOW Cloning Openbox-DE repository into: $PROXDIR/Openbox-DE $RC"
+	git clone https://github.com/senthilmask80/testing001.git "$PROXDIR/Openbox-DE"
+if [ $? -eq 0 ]; then
+	echo "$GREEN Successfully cloned Openbox-DE repository $RC"
+else
+    	echo "$RED Failed to clone Openbox-DE repository $RC"
+exit 1
+fi
+
+cd "$PROXDIR/Openbox-DE/Openbox-DE" || exit
+maindir=$PWD
+
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
