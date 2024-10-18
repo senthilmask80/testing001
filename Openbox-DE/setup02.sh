0cbe60586cd978400a17b693198bf8ab02659d52
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-18 09:59:22 +0530

    Update setup02.sh

--- Openbox-DE/setup02.sh
+++ Openbox-DE/setup02.sh
@@ -20,18 +20,18 @@ mkdir -p /usr/share/backgrounds
 mkdir -p $HOME/.nvm
 
 if [ ! -d "$PROXDIR" ]; then
-    echo "$YELLOW Creating Proxmox-DE directory: $PROXDIR $RC"
+    echo "${YELLOW}Creating Proxmox-DE directory: $PROXDIR${RC}"
     mkdir -p "$PROXDIR"
-    echo "$GREEN Proxmox-DE directory created: $PROXDIR $RC"
+    echo "${GREEN}Proxmox-DE directory created: $PROXDIR${RC}"
 fi
 
 if [ -d "$PROXDIR/Openbox-DE" ]; then rm -rf "$PROXDIR/Openbox-DE"; fi
-	echo "$YELLOW Cloning Openbox-DE repository into: $PROXDIR/Openbox-DE $RC"
+	echo "${YELLOW}Cloning Openbox-DE repository into: $PROXDIR/Openbox-DE${RC}"
 	git clone https://github.com/senthilmask80/testing001.git "$PROXDIR/Openbox-DE"
 if [ $? -eq 0 ]; then
-	echo "$GREEN Successfully cloned Openbox-DE repository $RC"
+	echo "${GREEN}Successfully cloned Openbox-DE repository${RC}"
 else
-    	echo "$RED Failed to clone Openbox-DE repository $RC"
+    	echo "${RED}Failed to clone Openbox-DE repository${RC}"
 exit 1
 fi
 
@@ -41,3 +41,25 @@ maindir=$PWD
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
+
+packagesNeeded=(curl jq)
+if [ -x "$(command -v apk)" ];
+then
+    sudo apk add --no-cache "${packagesNeeded[@]}"
+elif [ -x "$(command -v apt-get)" ];
+then
+    sudo apt update && sudo apt upgrade -y
+    sudo apt-get -y --ignore-missing install $(< $PRODIR/Openbox-DE/Openbox-DE/Scripts/debian-packages.list)
+    sudo apt autoclean && sudo apt autoremove -y && sudo rm -rf /var/cache/apt/archives/*
+elif [ -x "$(command -v dnf)" ];
+then
+    sudo dnf update -y 
+    sudo dnf install "${packagesNeeded[@]}"
+    sudo dnf clean all && sudo dnf autoremove -y 
+elif [ -x "$(command -v zypper)" ];
+then
+    sudo zypper install "${packagesNeeded[@]}"
+else
+    echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: "${packagesNeeded[@]}"">&2;
+fi
+
