3959c5a41be8d66f59353f1e0858fd81a1495fa5
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-19 22:52:18 +0530

    Update setup02.sh

--- Openbox-DE/setup02.sh
+++ Openbox-DE/setup02.sh
@@ -8,11 +8,17 @@ GREEN='\033[0;32m'
 # Check if the opt directory and Proxmox-DE folder exist, create them if they don't
 # add variables to top level so can easily be accessed by all functions
 
-PROXDIR="/opt/Proxmox-DE"
+PROXDIR="/tmp"
 SUDO_CMD=""
 SUGROUP=""
-GITPATH="$PROXDIR/Openbox-DE/Openbox-DE/bash"
+GITPATH="$PROXDIR/Openbox-DE/bash"
 mkdir -p /usr/share/backgrounds
+mkdir -p $HOME/.local/bin/
+mkdir -p $HOME/.config/obmenu-generator
+mkdir -p $HOME/.config/fbmenugen
+mkdir -p $HOME/.nvm
+mkdir -p $HOME/.fluxbox
+touch $HOME/.fluxbox/menu
 
 if [ ! -d "$PROXDIR" ]; then
     echo "${YELLOW}Creating Proxmox-DE directory: $PROXDIR${RC}"
@@ -30,8 +36,7 @@ else
 exit 1
 fi
 
-cd "$PROXDIR/Openbox-DE/Openbox-DE" || exit
-maindir=$PWD
+cd "$PROXDIR/Openbox-DE" || exit
 
 command_exists() {
     command -v "$1" >/dev/null 2>&1
@@ -44,7 +49,7 @@ then
 elif [ -x "$(command -v apt-get)" ];
 then
     sudo apt update && sudo apt upgrade -y
-    sudo apt-get -y --ignore-missing install $(< $PROXDIR/Openbox-DE/Openbox-DE/Scripts/debian-packages.list)
+    sudo apt-get -y --ignore-missing install $(< $PROXDIR/Openbox-DE/Scripts/debian-packages.list)
     sudo apt autoclean && sudo apt autoremove -y && sudo rm -rf /var/cache/apt/archives/*
     # To enable and active the services
     xdg-user-dirs-update
@@ -69,23 +74,23 @@ fi
 
 install_Packages() {
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/Openbox-DE/Openbox-DE/Packages/fastfetch-linux-amd64.deb
+    sudo apt-get install $PROXDIR/Openbox-DE/Packages/fastfetch-linux-amd64.deb
     sudo apt-get install -f
     echo "Successfully installed the fastfetch"
     
     # Download the webkit-lightdm deb file
-    sudo apt-get install $PROXDIR/Openbox-DE/Openbox-DE/Packages/web-greeter-3.5.3-debian.deb
+    sudo apt-get install $PROXDIR/Openbox-DE/Packages/web-greeter-3.5.3-debian.deb
     sudo apt-get install -f
     echo "Successfully installed the web-greeter"
 			
     # Download the lightdm-webkit2-greeter deb file
-    sudo apt-get install $PROXDIR/Openbox-DE/Openbox-DE/Packages/lightdm-webkit2-greeter.deb
+    sudo apt-get install $PROXDIR/Openbox-DE/Packages/lightdm-webkit2-greeter.deb
     sudo apt-get install -f
     echo "Successfully installed the lightdm-webkit2-greeter"
 
     # Install the Lightdm-Webkit2-greeter source file
-    chmod +x $PROXDIR/Openbox-DE/Openbox-DE/Scripts/webkit2.sh
-    bash $PROXDIR/Openbox-DE/Openbox-DE/Scripts/webkit2.sh
+    chmod +x $PROXDIR/Openbox-DE/Scripts/webkit2.sh
+    bash $PROXDIR/Openbox-DE/Scripts/webkit2.sh
     chmod -R 755 /usr/share/lightdm-webkit/themes/
     # Set default lightdm-webkit2-greeter theme to litarvan
     sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = litarvan #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
@@ -113,17 +118,17 @@ install_Obmenu() {
 	curl -L http://cpanmin.us | perl - --sudo File::DesktopEntry
 	#Installation process: place the obmenu-generator file inside your PATH
     	#place the schema.pl file inside ~/.config/obmenu-generator/
-     	chmod +x $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/obmenu-generator
-      	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/obmenu-generator /usr/local/bin/obmenu-generator
- 	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/obmenu-generator/config.pl
-  	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/schema.pl $HOME/.config/obmenu-generator/schema.pl
+     	sudo chmod +x $PROXDIR/Openbox-DE/Packages/obmenu-generator/obmenu-generator
+      	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/obmenu-generator /usr/local/bin/obmenu-generator
+ 	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/obmenu-generator/config.pl
+  	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/schema.pl $HOME/.config/obmenu-generator/schema.pl
    
 	#Installation process: place the fbmenugen file inside your PATH
     	#place the schema.pl file inside ~/.config/fbmenugen/
-     	chmod +x $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/fbmenugen
-	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/fbmenugen /usr/local/bin/fbmenugen
- 	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/fbmenugen/config.pl
-  	cp $PROXDIR/Openbox-DE/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/fbmenugen/schema.pl
+     	sudo chmod +x $PROXDIR/Openbox-DE/Packages/obmenu-generator/fbmenugen
+	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/fbmenugen /usr/local/bin/fbmenugen
+ 	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/fbmenugen/config.pl
+  	sudo cp $PROXDIR/Openbox-DE/Packages/obmenu-generator/config.pl $HOME/.config/fbmenugen/schema.pl
     fi
 }
 
@@ -139,8 +144,8 @@ install_Chezmoi() {
 
 install_StarshipAndFzf() {
     # Install the Nerd Fonts
-    chmod +x $PROXDIR/Openbox-DE/Openbox-DE/Scripts/Nerd-Fonts.sh
-    bash $PROXDIR/Openbox-DE/Openbox-DE/Scripts/Nerd-Fonts.sh
+    chmod +x $PROXDIR/Openbox-DE/Scripts/Nerd-Fonts.sh
+    bash $PROXDIR/Openbox-DE/Scripts/Nerd-Fonts.sh
     echo "Successfully installed the Nerd Fonts"
     
     if command_exists starship; then
@@ -162,19 +167,19 @@ install_StarshipAndFzf() {
 
 install_blesh() {
 	curl -LO https://github.com/akinomyoga/ble.sh/releases/download/v0.4.0-devel3/ble-0.4.0-devel3.tar.xz
- 	tar xJf ble-0.4.0-devel3.tar.xz -C ~/.local/share/
-  	mv ~/.local/share/ble-0.4.0-devel3 ~/.local/share/blesh
+ 	tar xJf ble-0.4.0-devel3.tar.xz -C $HOME/.local/share/
+  	mv ~/.local/share/ble-0.4.0-devel3 $HOME/.local/share/blesh
   	# Add the following line near the top of ~/.bashrc
-	[[ $- == *i* ]] && source ~/.local/share/blesh/ble.sh --attach=none
+	[[ $- == *i* ]] && source $HOME/.local/share/blesh/ble.sh --attach=none
 	# Add the following line at the end of ~/.bashrc
 	[[ ${BLE_VERSION-} ]] && ble-attach
 }
 
 final() {
-	chmod +x /opt/Proxmox/Openbox-DE/Openbox-DE/bash/00-bashrc.sh
-	cp /opt/Proxmox-DE/Openbox-DE/Openbox-DE/bash/00-bashrc.sh /etc/profile.d/00-bashrc.sh
- 	cp /opt/Proxmox-DE/Openbox-DE/Openbox-DE/bash/config.jsonc /etc/profile.d/config.jsonc
-	cp /opt/Proxmox-DE/Openbox-DE/Openbox-DE/bash/starship.toml /etc/profile.d/starship.toml  
+	sudo chmod +x /tmp/Openbox-DE/Openbox-DE/bash/00-bashrc.sh
+	sudo cp /tmp/Openbox-DE/bash/00-bashrc.sh /etc/profile.d/00-bashrc.sh
+ 	sudo cp /tmp/Openbox-DE/bash/config.jsonc /etc/profile.d/config.jsonc
+	sudo cp /tmp/Openbox-DE/bash/starship.toml /etc/profile.d/starship.toml  
 }
 
 install_Packages
