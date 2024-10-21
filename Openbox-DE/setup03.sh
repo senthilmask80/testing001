9f937db539bcdcdca9a1ed20b32e26fe959c8d9c
Senthil Kumaran <53327340+senthilmask80@users.noreply.github.com>
2024-10-21 10:40:24 +0530

    Update setup03.sh

--- Openbox-DE/setup03.sh
+++ Openbox-DE/setup03.sh
@@ -80,50 +80,58 @@ install_Packages() {
     chmod +x $PROXDIR/Openbox-DE/Openbox-DE/Scripts/webkit2.sh
     bash $PROXDIR/Openbox-DE/Openbox-DE/Scripts/webkit2.sh
     chmod -R 755 /usr/share/lightdm-webkit/themes/
+    # Set default lightdm-webkit2-greeter theme to litarvan
+    sudo sed -i 's/^webkit_theme\s*=\s*\(.*\)/webkit_theme = litarvan #\1/g' /etc/lightdm/lightdm-webkit2-greeter.conf
+
+    # Set default lightdm greeter to lightdm-webkit2-greeter
+    #sudo sed -i 's/^\(#?greeter\)-session\s*=\s*\(.*\)/greeter-session = lightdm-webkit2-greeter #\1/ #\2g' /etc/lightdm/lightdm.conf
+    sed -i 's/^#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
+    sed -i 's/^#user-session=default/user-session=openbox/' /etc/lightdm/lightdm.conf
+    sed -i 's/^    theme: gruvbox/    theme: LightdmTheme/' /etc/lightdm/web-greeter.yml
     echo "Successfully installed the lightdm-webkit2-greeter"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/ProxDot/packages/bunsen-thunar_11.0-2_all.deb
+    sudo apt-get install -y $PROXDIR/ProxDot/packages/bunsen-thunar_11.0-2_all.deb
     sudo apt-get install -f
     echo "Successfully installed the bunsen-thunar"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/ProxDot/packages/fonts-jetbrains-mono_2.242+ds-2_all.deb
+    sudo apt-get install -y $PROXDIR/ProxDot/packages/fonts-jetbrains-mono_2.242+ds-2_all.deb
     sudo apt-get install -f
     echo "Successfully installed the fonts-jetbrains-mono"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/ProxDot/packages/gammy_0.9.64-1~ld1_amd64.deb
+    sudo apt-get install -y $PROXDIR/ProxDot/packages/gammy_0.9.64-1~ld1_amd64.deb
     sudo apt-get install -f
     echo "Successfully installed the gammy"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/ProxDot/packages/jgmenu_4.4.0-1_amd64.deb
+    sudo apt-get install -y $PROXDIR/ProxDot/packages/jgmenu_4.4.0-1_amd64.deb
     sudo apt-get install -f
     echo "Successfully installed the jqmenu"
 
     # Install the downloaded deb file using apt-get
-    #sudo apt-get install $PROXDIR/ProxDot/packages/nala-legacy_0.11.0_amd64.deb
+    #sudo apt-get install -y $PROXDIR/ProxDot/packages/nala-legacy_0.11.0_amd64.deb
     #sudo apt-get install -f
     #echo "Successfully installed the fastfetch"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/proxDot/Packages/obkey_22.10.16_all.deb
+    sudo apt-get install -y $PROXDIR/proxDot/Packages/obkey_22.10.16_all.deb
     sudo apt-get install -f
     echo "Successfully installed the obkey"
 
     # Install the downloaded deb file using apt-get
-    sudo apt-get install $PROXDIR/proxDot/Packages/fastfetch-linux-amd64.deb
+    sudo apt-get install -y $PROXDIR/proxDot/Packages/fastfetch-linux-amd64.deb
     sudo apt-get install -f
     echo "Successfully installed the fastfetch"
     
     # Download the webkit-lightdm deb file
-    sudo apt-get install $PROXDIR/proxDot/Packages/web-greeter-3.5.3-debian.deb
+    sudo apt-get install -y $PROXDIR/proxDot/Packages/web-greeter-3.5.3-debian.deb
     sudo apt-get install -f
     echo "Successfully installed the web-greeter"
 			
     # Download the lightdm-webkit2-greeter deb file
-    sudo apt-get install $PROXDIR/proxDot/Packages/lightdm-webkit2-greeter.deb
+    sudo apt-get install -y $PROXDIR/proxDot/Packages/lightdm-webkit2-greeter.deb
     sudo apt-get install -f
     echo "Successfully installed the lightdm-webkit2-greeter"
 
